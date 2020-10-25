f97392245716fd9f2f3ce2916426d91bef5276df
Yuri van Midden <me@yurivanmidden.nl>
2020-10-25 22:56:12 +0100

    Initial work, proof of concept
    Signed-off-by: Yuri van Midden <me@yurivanmidden.nl>

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Howl, Howler } from 'howler';
 
 export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
+
+  const lame = require('@suldashi/lame');
+  const Speaker = require('speaker');
+  const fs = require('fs');
+
+  const speaker = new Speaker({
+    channels: 2,
+    bitDepth: 16,
+    sampleRate: 44100
+  })
+
+  var decoder = new lame.Decoder();
+  var input = fs.createReadStream('pages/api/sounds/preview.mp3');
+  input.pipe(decoder).pipe(speaker);
+  
+
+  var sound = new Howl({
+    src: ['sounds/sound.mp3']
+  });
+
+  sound.play();
+  console.log('Play sound');
 }
