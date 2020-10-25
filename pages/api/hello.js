b0ad28fe7acf5f5ae7eb08cd7a328468b5b50e9b
Yuri van Midden <me@yurivanmidden.nl>
2020-10-26 00:04:59 +0100

    Remove unused packages

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,13 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Howl, Howler } from 'howler';
+import { time } from 'console';
 
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default (req, res) => {
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
 
   const lame = require('@suldashi/lame');
   const Speaker = require('speaker');
   const fs = require('fs');
+  const tags = require('jsmediatags');
 
   const speaker = new Speaker({
     channels: 2,
@@ -16,14 +16,33 @@ export default (req, res) => {
   })
 
   var decoder = new lame.Decoder();
-  var input = fs.createReadStream('pages/api/sounds/preview.mp3');
+  var input = fs.createReadStream('pages/api/sounds/erreur.mp3');
   input.pipe(decoder).pipe(speaker);
-  
 
-  var sound = new Howl({
-    src: ['sounds/sound.mp3']
+  tags.read('pages/api/sounds/erreur.mp3', { 
+    onSuccess: function(tag) {
+      console.log(tag);
+      // res.json({title: `${tag.tags.title}`})
+    }
   });
 
-  sound.play();
+  const audioDir = "pages/api/sounds/";
+
+  var allFiles = fs.readdirSync(`${audioDir}`);
+  var allTags = new Array();
+  // var now = time.now();
+  allFiles.map(file => {
+    tags.read(`${audioDir}${file}`, { 
+      onSuccess: function(tag) {
+        console.log(tag.tags.title);
+        allTags.push(tag.tags.title);
+        // res.json({title: `${tag.tags.title}`})
+      }
+    });
+  });
+    
+  res.json({songs: allTags});
+
+  
   console.log('Play sound');
 }
